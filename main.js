a59038b9fd09cb3593d678dd0472b4a352aa558c
William Guerra <williamtguerra@gmail.com>
2024-05-13 23:47:10 -0500

    draw triangles

--- main.js
+++ main.js
@@ -8,21 +8,35 @@ function setup() {
     createCanvas(canvasSize, canvasSize);
 }
 
-function drawSquare(x, y, s) {
+function tri(x, y, lr) {
+    const h = 100;
+    const w = 120;
+
     fill(rand(255), rand(255), rand(255));
-    rect(x, y, s, s);
+    noStroke();
+    beginShape();
+    if (lr) {
+        vertex(x, y);
+        vertex(x + w, y + h / 2);
+        vertex(x + w, y - h / 2);
+    } else  {
+        vertex(x + w, y);
+        vertex(x, y + h / 2);
+        vertex(x, y - h / 2);
+    }
+    endShape();
 }
 
-function draw36() {
-    const width = canvasSize / 6;
+function drawTriangles() {
+    const width = 120;
     for (let i = 0; i < 6; i++) {
-        for (let j = 0; j < 6; j++) {
-            drawSquare(i * width, j * width, width);
+        for (let j = 0; j < 12; j++) {
+            tri(i * width, j * 50 + 50, (i + j) % 2 === 0);
         }
     }
 }
 
 function draw() {
     noLoop();
-    draw36();
+    drawTriangles();
 }
