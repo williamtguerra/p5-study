cb1ccd70d0b864a0127532a26efee415e55c3ac2
William Guerra <williamtguerra@users.noreply.github.com>
2024-05-14 22:34:26 -0500

    Bent (#2)
    * draw triangles
    
    * getting bent
    
    * done with basics

--- main.js
+++ main.js
@@ -1,28 +1,104 @@
 const canvasSize = 600;
 
 function rand(n = 1) {
-    return Math.floor(Math.random() * n);
+    return n > 1 ? Math.floor(Math.random() * n) : Math.random();
 }
 
 function setup() {
     createCanvas(canvasSize, canvasSize);
 }
 
-function drawSquare(x, y, s) {
+function tri(x, y, lr, bend) {
+    const w = 120;
+    const h = w / 2;
+    const lw = 60;
+    const p = 0.2;
+
+    noStroke();
+
+    // bg
+    const grey = rand(10) + 60;
+    fill(grey, grey, grey);
+    beginShape();
+    if (true) {
+        if (lr) {
+            /*     <     */
+            vertex(x, y);
+            vertex(x + w, y - w / 2);
+            vertex(x + w, y + w / 2);
+        } else {
+            /*     >     */
+            vertex(x + w, y);
+            vertex(x, y - w / 2);
+            vertex(x, y + w / 2);
+        }
+    }
+    endShape();
+
+    // bend
     fill(rand(255), rand(255), rand(255));
-    rect(x, y, s, s);
+    beginShape();
+    if (bend === 0) {
+        if (lr) {
+            /*     < \     */
+            vertex(x + (w / 2 - lw / 2), y - lw / 4);
+            vertex(x + (w / 2 + lw / 2), y - (3 * lw) / 4);
+            vertex(x + w, y - lw / 2);
+            vertex(x + w, y + lw / 2);
+        } else {
+            /*     > \     */
+            vertex(x, y - lw / 2);
+            vertex(x + (w / 2 + lw / 2), y + lw / 4);
+            vertex(x + (w / 2 - lw / 2), y + (3 * lw) / 4);
+            vertex(x, y + lw / 2);
+        }
+    } else if (bend === 1) {
+        if (lr) {
+            /*     < /     */
+            vertex(x + (w - lw) / 2, y + lw / 4);
+            vertex(x + w, y - lw / 2);
+            vertex(x + w, y + lw / 2);
+            vertex(x + (w - lw) / 2 + lw, y + (3 * lw) / 4);
+        } else {
+            /*     > /     */
+            vertex(x + (w - lw) / 2, y - (3 * lw) / 4);
+            vertex(x + (w - lw) / 2 + lw, y - lw / 4);
+            vertex(x, y + lw / 2);
+            vertex(x, y - lw / 2);
+        }
+    } else {
+        if (lr) {
+            /*     < |     */
+            vertex(x + (w - lw) / 2, y - lw / 4);
+            vertex(x + (w - lw) / 2 + lw, y - (3 * lw) / 4);
+            vertex(x + (w - lw) / 2 + lw, y + (3 * lw) / 4);
+            vertex(x + (w - lw) / 2, y + lw / 4);
+        } else {
+            /*     > |     */
+            vertex(x + (w - lw) / 2, y - (3 * lw) / 4);
+            vertex(x + (w - lw) / 2 + lw, y - lw / 4);
+            vertex(x + (w - lw) / 2 + lw, y + lw / 4);
+            vertex(x + (w - lw) / 2, y + (3 * lw) / 4);
+        }
+    }
+    endShape();
 }
 
-function draw36() {
-    const width = canvasSize / 6;
+function drawTriangles() {
+    const width = 120;
     for (let i = 0; i < 6; i++) {
-        for (let j = 0; j < 6; j++) {
-            drawSquare(i * width, j * width, width);
+        for (let j = 0; j < 12; j++) {
+            tri(
+                i * width,
+                j * (width / 2) + width / 2,
+                (i + j) % 2 === 0,
+                rand(12)
+            );
         }
     }
 }
 
 function draw() {
     noLoop();
-    draw36();
+    drawTriangles();
 }
