7797d4feca4204c93e9a5db52871ae7e46f4c134
William Guerra <williamtguerra@gmail.com>
2024-05-16 01:52:36 -0500

    default file

--- main.js
+++ main.js
@@ -4,101 +4,23 @@ function rand(n = 1) {
 
 function setup() {
     const canvasSize = 600;
-    createCanvas(canvasSize, canvasSize * 3);
+    createCanvas(canvasSize, canvasSize);
 }
 
-function tri(x, y, lr, bend) {
-    const w = 120; // triangle width
-    const t = 55; // line thickness
+function drawGrid() {
+    const col = 6;
+    const row = 6;
+    const x = 100;
 
-    noStroke();
-
-    // bg triangles
-    const grey = rand(10) + 60;
-    fill(grey);
-    beginShape();
-    if (true) {
-        if (lr) {
-            /*     <     */
-            vertex(x + w, y);
-            vertex(x, y + w / 2);
-            vertex(x + w, y + w);
-        } else {
-            /*     >     */
-            vertex(x, y);
-            vertex(x + w, y + w / 2);
-            vertex(x, y + w);
-        }
-    }
-    endShape();
-
-    // bend
-    beginShape();
-    if (bend === 0) {
-        fill(180);
-        if (lr) {
-            /*     < \     */
-            vertex(x + (w - t) / 2, y + (w + t) / 4);
-            vertex(x + (w + t) / 2, y + (w - t) / 4);
-            vertex(x + w, y + (w - t) / 2);
-            vertex(x + w, y + (w + t) / 2);
-        } else {
-            /*     \ >     */
-            vertex(x, y + (w - t) / 2);
-            vertex(x + (w + t) / 2, y + w - (w + t) / 4);
-            vertex(x + (w - t) / 2, y + w - (w - t) / 4);
-            vertex(x, y + (w + t) / 2);
-        }
-    } else if (bend === 1) {
-        fill(180);
-        if (lr) {
-            /*     < /     */
-            vertex(x + w, y + (w - t) / 2);
-            vertex(x + w, y + (w + t) / 2);
-            vertex(x + (w + t) / 2, y + w - (w - t) / 4);
-            vertex(x + (w - t) / 2, y + w - (w + t) / 4);
-        } else {
-            /*     / >     */
-            vertex(x, y + (w - t) / 2);
-            vertex(x + (w - t) / 2, y + (w - t) / 4);
-            vertex(x + (w + t) / 2, y + (w + t) / 4);
-            vertex(x, y + (w + t) / 2);
-        }
-    } else {
-        fill(240);
-        if (lr) {
-            /*     < |     */
-            vertex(x + (w - t) / 2, y + (w + t) / 4);
-            vertex(x + (w + t) / 2, y + (w - t) / 4);
-            vertex(x + (w + t) / 2, y + w - (w - t) / 4);
-            vertex(x + (w - t) / 2, y + w - (w + t) / 4);
-        } else {
-            /*     | >     */
-            vertex(x + (w - t) / 2, y + (w - t) / 4);
-            vertex(x + (w + t) / 2, y + (w + t) / 4);
-            vertex(x + (w + t) / 2, y + w - (w + t) / 4);
-            vertex(x + (w - t) / 2, y + w - (w - t) / 4);
-        }
-    }
-
-    // color override
-    // fill(rand(255), rand(255), rand(255));
-
-    endShape();
-}
-
-function drawTriangles() {
-    const width = 120;
-    for (let i = 0; i < 6; i++) {
-        // columns
-        for (let j = 0; j < 24; j++) {
-            // rows
-            tri(i * width, (j * width) / 2, (i + j) % 2 === 0, rand(12));
+    for (let i = 0; i < col; i++) {
+        for (let j = 0; j < row; j++) {
+            fill(200 + rand(50));
+            rect(i * x, j * x, x);
         }
     }
 }
 
 function draw() {
     noLoop();
-    drawTriangles();
+    drawGrid();
 }
